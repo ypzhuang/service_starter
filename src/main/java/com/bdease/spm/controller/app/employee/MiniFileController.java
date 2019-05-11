56ec9ff3b0923317321c695fc969fd9f5753a521
John Zhuang <yinping.zhuang@hp.com>
2019-05-11 16:33:05 +0800

    file upload for emp

--- src/main/java/com/bdease/spm/controller/app/employee/MiniFileController.java
+++ src/main/java/com/bdease/spm/controller/app/employee/MiniFileController.java
@@ -1,13 +1,6 @@
-/**
- * created Jan 12, 2019 by ypzhuang
- * 
- * 上传文件或者Base64图片
- */
-
-package com.bdease.spm.controller;
+package com.bdease.spm.controller.app.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,19 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.bdease.spm.controller.app.MiniBaseController;
 import com.bdease.spm.service.IFileService;
 import com.bdease.spm.vo.FilePathResponse;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-
 @RestController
-@RequestMapping("/api/v1/files")
-@Api(tags = {"File"})
-@PreAuthorize("hasAnyRole('ROLE_SUPER_ADMIN','ROLE_MANAGER')")
-public class FileController extends BaseController {
-	 
+@RequestMapping("/app/emp/v1/files")
+@Api(tags={"MiniEmp"})
+@PreAuthorize("hasAnyRole('ROLE_SHOP_USER','ROLE_SHOP_ADMIN')")
+public class MiniFileController extends MiniBaseController {
 	@Autowired
 	private IFileService fileService;		
 	
@@ -50,4 +42,3 @@ public class FileController extends BaseController {
     	return this.fileService.uploadBase64Picture(base64);
     }    
 }
-
