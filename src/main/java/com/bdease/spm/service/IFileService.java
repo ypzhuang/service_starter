2d3f068e7bcda6b6f6e6a5b5b77f046de7d3bf84
John Zhuang <yinping.zhuang@hp.com>
2019-05-11 16:28:40 +0800

    refactor file upload

--- src/main/java/com/bdease/spm/service/IFileService.java
+++ src/main/java/com/bdease/spm/service/IFileService.java
@@ -6,8 +6,16 @@
 
 package com.bdease.spm.service;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+
+import org.springframework.web.multipart.MultipartFile;
+import com.bdease.spm.vo.FilePathResponse;
+
 
 public interface IFileService {
 	
@@ -15,7 +23,18 @@ public interface IFileService {
 	String EXTENSION = "extension";
 	String SIZE = "size";
 	
+	String[] SUPPORT_IMAGE_TYPE = { "JPG", "JPEG", "PNG", "GIF", "BMP", "WBMP" };
+	List<String> SUPPORT_IMAGE_LIST = Arrays.asList(SUPPORT_IMAGE_TYPE);	
+	
 	String uploadFile(InputStream inputStream, long fileSize, String fileExtName, Map<String,String> meta);
 	
 	String uploadFile(InputStream inputStream, long fileSize, String fileExtName);
+	
+	String uploadFile(InputStream inputStream, long fileSize, String filename, String fileExtension) throws Exception;
+	
+	FilePathResponse uploadCommonFile(MultipartFile file) throws Exception, IOException;
+	
+	FilePathResponse uploadPictureFile(MultipartFile file) throws Exception;	
+	
+	FilePathResponse uploadBase64Picture(String base64) throws Exception, FileNotFoundException, IOException;
 }
--- src/main/java/com/bdease/spm/service/impl/FastDFSFileService.java
+++ src/main/java/com/bdease/spm/service/impl/FastDFSFileService.java
@@ -6,30 +6,48 @@
 
 package com.bdease.spm.service.impl;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
-
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.http.util.Asserts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.bdease.spm.ex.ApplicationException;
+import com.bdease.spm.security.JwtUser;
 import com.bdease.spm.service.IFileService;
+import com.bdease.spm.utils.FileHelper;
+import com.bdease.spm.vo.FilePathResponse;
 import com.github.tobato.fastdfs.domain.MetaData;
 import com.github.tobato.fastdfs.domain.StorePath;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
 
 @Service
-public class FastDFSFileService implements IFileService {
+public class FastDFSFileService implements IFileService {	
+	
 	protected  final Logger log = LoggerFactory.getLogger(getClass());
 	
-	 @Autowired
-	 private FastFileStorageClient fastClient;
+	@Autowired
+	private FastFileStorageClient fastClient;
+
+	@Value("${file.tmp-delete}")
+	private boolean isTmpDelete;
 
 	@Override
 	public String uploadFile(InputStream inputStream, long fileSize, String fileExtName, Map<String, String> meta) {
@@ -51,4 +69,63 @@ public class FastDFSFileService implements IFileService {
 	public String uploadFile(InputStream inputStream, long fileSize, String fileExtName) {
 		return this.uploadFile(inputStream, fileSize, fileExtName,new HashMap<String,String>());
 	}
+	
+	@Override
+	public FilePathResponse uploadCommonFile(MultipartFile file) throws Exception, IOException {
+		String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+    	String filename =  FilenameUtils.getBaseName(file.getOriginalFilename());    	
+    	String path = uploadFile(file.getInputStream(),file.getSize(),filename,fileExtension); 
+    	return new FilePathResponse(path);
+	}    
+	
+	@Override
+	public String uploadFile(InputStream inputStream,long fileSize,String filename,String fileExtension) throws Exception {    	
+    	Map<String,String> meta = new HashMap<String,String>();
+    	meta.put(IFileService.FILE_NAME, filename);
+    	meta.put(IFileService.EXTENSION, fileExtension);
+    	meta.put(IFileService.SIZE,String.valueOf(fileSize));     
+    	return this.uploadFile(inputStream, fileSize, fileExtension,meta);      
+    }
+    
+    private String getOriginPictureType(String base64) {
+    	if( base64.startsWith("data:image/jpeg;base64")) {
+    		return "jpg";
+    	} else if (base64.startsWith("data:image/png;base64")) {
+    		return "png";
+    	}     	
+    	throw new ApplicationException("不支持的base64格式，当前只支持png后者jpg图片");
+	}
+
+  	 private boolean isSupportImage(String fileExtName) {
+  	        return SUPPORT_IMAGE_LIST.contains(fileExtName.toUpperCase());
+  	 }
+  	
+  	@Override
+  	public FilePathResponse uploadPictureFile(MultipartFile file) throws Exception {
+		String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+    	Asserts.check(isSupportImage(fileExtension), "不支持的图片格式,%s" , fileExtension);    	
+    	return uploadCommonFile(file);
+	}
+  	
+  	@Override
+  	public FilePathResponse uploadBase64Picture(String base64) throws Exception, FileNotFoundException, IOException {
+		String fileExtension = this.getOriginPictureType(base64);
+    	String fileName = JwtUser.currentUserId() < 0L? String.valueOf(JwtUser.currentUserId()) : String.valueOf(new Date().getTime());
+    	try (InputStream stream = FileHelper.base64ToInputSteam(base64)) {     		
+    		File file = writeToTempFile(stream, fileExtension);
+    		FileInputStream inputStream = new FileInputStream(file);
+    		String path = this.uploadFile(inputStream, file.length(), fileName ,fileExtension);
+    		inputStream.close();
+    		if(isTmpDelete) {
+    			file.delete();
+    		}
+    		return new FilePathResponse(path);
+    	}
+	}
+  	
+	private File writeToTempFile(InputStream stream, String fileExtension) throws Exception {
+		File file = File.createTempFile("fjsj", fileExtension);
+		Files.copy(stream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		return file;
+	}  
 }
